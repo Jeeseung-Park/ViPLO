680254df2fcf11010ee292a5f15e1fa900421555
Jeeseung-Park <inuyashajs@snu.ac.kr>
2023-04-16 13:28:32 +0000

    change download checkpoint

--- download/download_checkpoint.sh
+++ download/download_checkpoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-DIR=../checkpoints
-FILE=scg_1e-4_b32h16e7_hicodet_e2e.pt
-ID=1yOebBfiNIR20EyAZGap88ecUgMR8J0X_
+DIR=checkpoints
+FILE=best_hicodet.pt
+ID=1NRP05WDdjLW1MBMRnBU5LgcVmyOXp-wK
 
 if [ ! -d $DIR ]; then
    mkdir $DIR
@@ -15,7 +15,7 @@ fi
 
 echo "Connecting..."
 
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&id=$ID" -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1/p')&id=$ID" -O $FILE && rm -rf /tmp/cookies.txt
+gdown $ID
 
 mv $FILE $DIR/
 
